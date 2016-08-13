d15a9f986ad7dd6d20e95f1b269bb870bc2bca14
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-08-13 21:21:59 +1000

    Add LED component

---
+++ src/components/LED.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// This is an LED class that will be used to place LEDs around the screen
+// These LEDs should have their color updated via some inline CSS
+class LED extends Component {
+  render() {
+    <div className="LED" />
+  }
+}
+
+export default LED;
