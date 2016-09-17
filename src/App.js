f002496672883f11e18baa128402b51ec72dbd8f
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-09-17 17:03:55 +1000

    Add LEDContainer to app
    This adds the LEDContainer element to the app.js, as well as some css
    styling to make it behave how we want.
    
    For testing I'm including a few LEDs of different sizes and at random
    locations, but in the future these LEDs will be fed from some sort of
    input data stream.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import Color from 'color';
+import LEDContainer from './components/LEDContainer.js';
 import './App.css';
+
+var ledArray = [
+  {
+    id: 0,
+    size: 60,
+    color: Color().blue(255),
+    left: 30,
+    top: 100
+  },
+  {
+    id: 1,
+    size: 30,
+    color: Color().red(120),
+    left: 200,
+    top: 600
+  },
+  {
+    id: 2,
+    size: 45,
+    color: Color().green(20),
+    left: 600,
+    top: 800
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LEDContainer leds={ledArray}/>
       </div>
     );
   }
