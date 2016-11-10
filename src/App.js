c914b11938085d4f68cb17231b353e71f5926656
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-11-10 22:49:52 +1100

    Redesigning, starting again almost
    This constitutes a bit of a redesign. For performance, ease of use,
    experience and extensibility I'm going to draw the LEDs using webgl.
    I'm also starting from a top-down definition of components to help with
    the layout.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React, { Component } from 'react';
-import Color from 'color';
-import LEDContainer from './components/LEDContainer.js';
 import './App.css';
 
-var numLeds = 9;
-var ledArray = [];
-for (var i = 0; i < numLeds; i++) {
-  var col = Color().red(255 * Math.random()).green(255 * Math.random()).green(255 * Math.random());
-  ledArray.push({
-    id: i,
-    color: col
-  });
-}
-
 var App = React.createClass({
-  alterColors: function() {
-    ledArray.forEach((ledObj, index, array) => {
-      ledObj.color = ledObj.color.clone().rotate(1);
-    });
-    this.setState({leds: ledArray});
-  },
-
-  getInitialState: function() {
-      return {
-        leds: ledArray,
-        layout: 'concentric'
-      };
-  },
-
-  componentDidMount: function() {
-    setInterval(this.alterColors, 10);
-  },
-
   render: function() {
     return (
       <div className="App">
-        <LEDContainer leds={this.state.leds} layout={this.state.layout}/>
+        <div className="column-1 column">
+        </div>
+        <div className="column-2 column">
+        </div>
       </div>
     );
   }
