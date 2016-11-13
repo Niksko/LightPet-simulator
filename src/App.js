8a3cd12a776fdaded06b2799b7a3e79dc2e11610
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-11-13 19:32:21 +1100

    Add back LED generation and passing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import Color from 'color';
 import DataTimeline from './components/DataTimeline.js';
 import SettingsPanel from './components/SettingsPanel.js';
 import SimulationInput from './components/SimulationInput.js';
 import SimulationOutput from './components/SimulationOutput.js';
 import './App.css';
 
-var App = React.createClass({
+let numLeds = 9;
+let ledArray = [];
+for (let i = 0; i < numLeds; i++) {
+  let col = Color().red(255 * Math.random()).green(255 * Math.random()).green(255 * Math.random());
+  ledArray.push(col);
+}
+
+let App = React.createClass({
   render: function() {
     return (
-      <div className="App">
-        <div className="column-1 column">
-          <SimulationOutput/>
+      <div className='App'>
+        <div className='column-1 column'>
+          <SimulationOutput leds={ledArray} layout='concentric'/>
           <DataTimeline/>
         </div>
-        <div className="column-2 column">
+        <div className='column-2 column'>
           <SettingsPanel/>
           <SimulationInput/>
         </div>
