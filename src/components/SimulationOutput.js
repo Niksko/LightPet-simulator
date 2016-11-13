f7f30c6bae3d659c237a450422a1344c3963caa3
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-11-13 19:42:51 +1100

    Rip out react-pixi
    Docs aren't up to scratch, not ready from prime time. It's going to be
    much more hassle than it's worth.

--- src/components/SimulationOutput.js
+++ src/components/SimulationOutput.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import ReactPIXI from 'react-pixi';
 import PIXI from 'pixi.js';
-
-var Stage = ReactPIXI.Stage;
 
 var SimulationOutput = React.createClass({
   render: function() {
     return (
       <div className="SimulationOutput main-component">
-        <Stage width={this.props.width} height={this.props.height}>
-        </Stage>
       </div>
     );
   }
