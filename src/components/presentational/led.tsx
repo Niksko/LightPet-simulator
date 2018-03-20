7e0178f5a3bda6bec92447afeec91c99442e4937
Nikolas M Skoufis <n.skoufis@gmail.com>
2018-03-20 18:06:58 +1100

    Create basic led component
    Create a basic LED presentational component that accepts a size, x, y
    and color props. The result is a Konva group that contains a Circle and
    Rect that create the basic shape of an LED.

---
+++ src/components/presentational/led.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Circle, Rect, Group } from 'react-konva';
+
+export interface Props {
+  size: number;
+  x: number;
+  y: number;
+  color: string;
+}
+
+function LED({size, x, y}: Props) {
+  return (
+    <Group
+      x={x}
+      y={y}
+    >
+      <Circle
+        width={size}
+        height={size}
+        fill={color}
+        x={0}
+        y={0}
+      />
+      <Rect
+        width={size}
+        height={size}
+        fill={color}
+        x={-size / 2}
+        y={0}
+      />
+    </Group>
+  );
+}
