dd7a2f954a261d8eed8538737aac2faa0460ccd1
Nikolas M Skoufis <n.skoufis@gmail.com>
2018-04-07 21:44:51 +1000

    Add wiring to enable update button functionality
    This adds a property on the ledTextbox that accepts a function which
    will be called with the new number of leds to display. As part of this,
    we rewrite our parsing behaviour to parse the value that has been
    entered as an integer, and display an error message (via a state field)
    if what was entered couldn't be parsed.
    
    Currently the parsing method is no good (will parse things like 20abc
    as 20), so we need to explore other options for integer parsing.

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,17 @@ import LedTextbox from './components/container/ledTextbox';
 const holderWidth = 500;
 const holderHeight = 500;
 
-let ledArray: Array<LEDData> = [];
-for (let i = 0; i < 10; i++) {
-  ledArray.push({
-    x: Math.random() * holderWidth,
-    y: Math.random() * holderHeight,
-    color: Konva.Util.getRandomColor()
-  });
-}
+const generateLedArray = (numberOfLeds: number): Array<LEDData> => {
+  let ledArray: Array<LEDData> = [];
+  for (let i = 0; i < numberOfLeds; i++) {
+    ledArray.push({
+      x: Math.random() * holderWidth,
+      y: Math.random() * holderHeight,
+      color: Konva.Util.getRandomColor()
+    });
+  }
+  return ledArray;
+};
 
 let dummyData: Array<DataPoint> = [];
 for (let i = 0; i < 100; i++) {
@@ -31,23 +34,36 @@ export interface Props {
 export interface State {
   dataStart: number;
   dataEnd: number;
+  ledArray: Array<LEDData>;
 }
 
 class App extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { dataStart: 0, dataEnd: 100 };
+    this.state = {
+      dataStart: 0,
+      dataEnd: 100,
+      ledArray: generateLedArray(10)
+    };
+    this.updateNumberOfLeds = this.updateNumberOfLeds.bind(this);
   }
+
   render() {
     return (
       <div className="App">
         <div>
-          <LEDHolder width={holderWidth} height={holderHeight} ledSize={30} leds={ledArray}/>
-          <LedTextbox initialNumberOfLeds={20}/>
+          <LEDHolder width={holderWidth} height={holderHeight} ledSize={30} leds={this.state.ledArray}/>
+          <LedTextbox initialNumberOfLeds={20} updateNumberOfLeds={this.updateNumberOfLeds}/>
         </div>
         <GraphView width={400} height={400} data={dummyData} />
       </div>
     );
+  }
+
+  private updateNumberOfLeds(newNumberOfLeds: number): void {
+    this.setState({
+      ledArray: generateLedArray(newNumberOfLeds)
+    });
   }
 }
 
--- src/components/container/ledTextbox.tsx
+++ src/components/container/ledTextbox.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, MouseEvent } from 'react';
 
 export interface Props {
   initialNumberOfLeds: number;
+  updateNumberOfLeds: (newValue: number) => void;
 }
 
 export interface State {
   currentNumberOfLeds: string;
+  errorMessage: string;
 }
 
 class LedTextbox extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { currentNumberOfLeds: props.initialNumberOfLeds.toString() };
+    this.state = {
+      currentNumberOfLeds: props.initialNumberOfLeds.toString(),
+      errorMessage: ''
+    };
     this.handleTextboxChange = this.handleTextboxChange.bind(this);
+    this.handleUpdateClick = this.handleUpdateClick.bind(this);
   }
 
   render() {
@@ -24,7 +30,14 @@ class LedTextbox extends React.Component<Props, State> {
           value={this.state.currentNumberOfLeds}
           onChange={this.handleTextboxChange}
         />
-        <button>Update</button>
+        <button
+          onClick={this.handleUpdateClick}
+        >
+          Update
+        </button>
+        <div>
+          {this.state.errorMessage}
+        </div>
       </div>
     );
   }
@@ -33,6 +46,21 @@ class LedTextbox extends React.Component<Props, State> {
     this.setState({
       currentNumberOfLeds: event.target.value
     });
+  }
+
+  private handleUpdateClick(event: MouseEvent<HTMLButtonElement>): void {
+    const updatedNumberOfLeds = parseInt(this.state.currentNumberOfLeds, 10);
+
+    if (isNaN(updatedNumberOfLeds)) {
+      this.setState({
+        errorMessage: 'Unable to parse your input as an integer. Please enter integers.'
+      });
+    } else {
+      this.setState({
+        errorMessage: ''
+      });
+      this.props.updateNumberOfLeds(updatedNumberOfLeds);
+    }
   }
 }
 
